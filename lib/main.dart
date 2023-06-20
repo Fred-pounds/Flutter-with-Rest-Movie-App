af67d96557bae977f57a2988433ee3bdc49fb234
Fred-pounds <fredrickandoh17@gmail.com>
2023-06-20 01:18:25 +0000

    new additions to the movie app project

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,30 @@ import 'package:flutter/material.dart';
 
 //Pages
 import './Pages/splash_page.dart';
+import './Pages/main_page.dart';
 
 void main() {
   runApp(
-    SplashPage(key: UniqueKey(), onInitializationComplete: () => null),
+    SplashPage(
+        key: UniqueKey(),
+        onInitializationComplete: () => runApp(MyApp as Widget)),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flicked',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => const MainPage(),
+      },
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+    );
+  }
 }
